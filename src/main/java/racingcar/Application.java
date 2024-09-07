468d82b71cc7dd6862ce4f67cc233280779dca2b
player7571 <pxy7571@gmail.com>
2024-09-07 18:57:38 +0900

    feat: controller 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,15 +3,7 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        View view = new View();
-        Model model = new Model();
-
-        model.saveCarName(view.inputCarName());
-
-        model.saveNumberOfAttempts(view.inputNumberOfAttempts());
-
-        view.printResult(model.race());
-
-        view.printWinner(model.findWinner());
+        Controller controller = new Controller();
+        controller.run();
     }
 }
