1d1613569e4022273bf06b35f25f9924442ede31
legendary_octopus <story3798@naver.com>
2024-09-07 15:17:46 +0900

    Update : 불필요한 변수 삭제

--- src/main/java/racingcar/model/RandomNumber.java
+++ src/main/java/racingcar/model/RandomNumber.java
@@ -2,9 +2,6 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumber {
-    private static final int MAX_NUMBER = 10;
-    private static final int MIN_NUMBER = 0;
-
     public static int getRandomNumber() {
         return Randoms.pickNumberInRange(0,9);
     }
