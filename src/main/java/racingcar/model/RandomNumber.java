ca2e608521b22d56a1e34d5a8459cc747d8ef50a
legendary_octopus <story3798@naver.com>
2024-09-07 15:12:54 +0900

    Update : 우테코 제공 함수로 변경

--- src/main/java/racingcar/model/RandomNumber.java
+++ src/main/java/racingcar/model/RandomNumber.java
@@ -1,10 +1,11 @@
 package racingcar.model;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumber {
     private static final int MAX_NUMBER = 10;
     private static final int MIN_NUMBER = 0;
 
     public static int getRandomNumber() {
-        return (int) (Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
+        return Randoms.pickNumberInRange(0,9);
     }
 }
